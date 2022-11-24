1410361fa2ae51e4b09961a8d3d9d28a5f699a06
HiImZsofi <zsofia.gyori00@gmail.com>
2022-11-24 08:58:31 +0100

    fetch data from api

--- src/main/java/com/example/gyorizsofia_javafxrestclientdolgozat/ListJobs.java
+++ src/main/java/com/example/gyorizsofia_javafxrestclientdolgozat/ListJobs.java
@@ -1,9 +1,16 @@
 package com.example.gyorizsofia_javafxrestclientdolgozat;
 
+import com.google.gson.Gson;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.Optional;
 
 public class ListJobs extends ProjectController{
     @FXML
@@ -20,4 +27,31 @@ public class ListJobs extends ProjectController{
     public TableColumn locationCol;
     @FXML
     public TableColumn nameCol;
+
+    @FXML
+    private void initialize() {
+        // getName() függvény eredményét írja ki
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("jobName"));
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("jobTitle"));
+        locationCol.setCellValueFactory(new PropertyValueFactory<>("jobLocation"));
+        Platform.runLater(() -> {
+            try {
+                fetchData();
+            } catch (IOException e) {
+                error("Couldn't get data from server", e.getMessage());
+                Platform.exit();
+            }
+        });
+    }
+
+    private void fetchData() throws IOException {
+        Response response = RequestHandler.get(App.BASE_URL);
+        String content = response.getContent();
+        Gson converter = new Gson();
+        Job[] jobs = converter.fromJson(content, Job[].class);
+        jobTable.getItems().clear();
+        for (Job job : jobs) {
+            jobTable.getItems().add(job);
+        }
+    }
 }
