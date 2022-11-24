72568a98c686113075165d18e7d55a593d0951d7
HiImZsofi <zsofia.gyori00@gmail.com>
2022-11-24 08:24:57 +0100

    list view

---
+++ src/main/java/com/example/gyorizsofia_javafxrestclientdolgozat/ProjectController.java
@@ -0,0 +1,35 @@
+package com.example.gyorizsofia_javafxrestclientdolgozat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ProjectController {
+    @FXML
+    public Button insertButton;
+    @FXML
+    public Button updateButton;
+    @FXML
+    public Button deleteButton;
+    @FXML
+    public TableView jobTable;
+    @FXML
+    public TableColumn titleCol;
+    @FXML
+    public TableColumn locationCol;
+    @FXML
+    public TableColumn nameCol;
+
+    @FXML
+    public void insertClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void updateClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void deleteClick(ActionEvent actionEvent) {
+    }
+}
